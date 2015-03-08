85e04aed67547ffb5e7c1f604a03449ec8174181
munhra <rafael.munhoz@gmail.com>
2015-03-08 10:33:21 -0300

    Starting ajax chapter

--- app/loja/js/sugestoes/sugestoes.js
+++ app/loja/js/sugestoes/sugestoes.js
@@ -1,20 +1,22 @@
 $("input[type=button]").click(function(event){
 	var inputSugestao = $("input[type=text]");
 	var sugestao = inputSugestao.val();
-	var nomeSugestao = $("<span>").addClass("votos").text("1 voto");
-	var votos = $("<span>").addClass("votos").appendTo(".sugestoes");
+	var nomeSugestao = $("<span>").addClass("sugestao").text(sugestao);
+	var votos = $("<span>").addClass("votos").text("1 voto");
 	$("<li>").append(nomeSugestao).append(votos).appendTo(".sugestoes");
 	inputSugestao.val("").focus();
 });
 
+$(".sugestoes").on("dbclick","li",function(event) {
+		$(this).remove();
+});
+
+/*
 $("input[type=button]").click(function(event){
 	var inputSugestao = $("input[type=text]");
 	var sugestoes = inputSugestao.val();
 	var nomeSugestao = $("<span>").addClass("votos").text("1 voto");
 	$("<li>").append(nomeSugestao).append(votos).appendTo(".sugestoes");
 	inputSugestao.val("").focus();
-});
+});*/
 
-$(".sugestoes li").dbclick(function(event){
-	$(this).remove();
-});
